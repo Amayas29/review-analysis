044842f1038b8a7f618fc6edad15ccda56ce98cb
Amayas29 <amayas29sd@gmail.com>
2023-02-07 14:02:19 +0100

    format and switch to english

--- iads/data.py
+++ iads/data.py
@@ -1,29 +1,28 @@
 import bson
 import csv
 import pandas as pd
+import os.path
 
 
 def read_data(filename):
     """
-    Fonction pour lire les données à partir d'un fichier BSON
+    Function to read data from a BSON file
     """
-
     bs = open(f'../trictrac_data/{filename}', 'rb').read()
 
-    # Liste pour stocker les données décodées
+    # List to store decoded data
     dicts = []
     for valid_dict in bson.decode_all(bs):
         dicts.append(valid_dict)
 
-    # Renvoi des données décodées
+    # Return decoded data
     return dicts
 
 
 def dump_dicts_to_csv(data, schema, filename):
     """
-    Fonction pour écrire les données dans un fichier CSV
+    Function to write data to a CSV file
     """
-
     with open(filename, 'w', newline='') as file:
         writer = csv.DictWriter(file, fieldnames=schema)
         writer.writeheader()
@@ -32,9 +31,8 @@ def dump_dicts_to_csv(data, schema, filename):
 
 def get_schema(data):
     """
-    Fonction pour obtenir le schéma des données
+    Function to get data schema
     """
-
     schema = set()
 
     for d in data:
@@ -45,25 +43,28 @@ def get_schema(data):
 
 def load_dataframe(filename):
     """
-    Fonction pour charger les données dans un dataframe
+    Function to load data into a dataframe
     """
-
-    # Obtention du nom de base du fichier
+    # Get base file name
     base_filename = filename.split(".")[0]
 
-    # Lecture des données à partir du fichier BSON
+    # If csv already exists
+    if os.path.exists(f"../data/{base_filename}.csv"):
+        return pd.read_csv(f"../data/{base_filename}.csv")
+
+    # Read data from BSON file
     data = read_data(filename)
 
-    # Obtention du schéma des données
+    # Get data schema
     schema = get_schema(data)
 
-    # Ecriture des données dans un fichier CSV
+    # Write data to CSV file
     dump_dicts_to_csv(data, schema, f"../data/{base_filename}.csv")
 
-    # Chargement des données dans un dataframe
+    # Load data into dataframe
     df = pd.read_csv(f"../data/{base_filename}.csv")
 
-    # Vérification de la cohérence des données
-    assert df.shape[0] == len(data), "Erreur lors du chargement des données"
+    # Check data consistency
+    assert df.shape[0] == len(data), "Error loading data"
 
     return df
