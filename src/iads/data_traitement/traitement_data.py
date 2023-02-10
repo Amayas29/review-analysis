f7023c5229ef909de3226c32713bcaf61e386989
Amayas29 <amayas29sd@gmail.com>
2023-02-10 01:42:38 +0100

    merge avis and categories et start stats

--- src/iads/data_traitement/traitement_data.py
+++ src/iads/data_traitement/traitement_data.py
@@ -2,11 +2,11 @@ import re
 import numpy as np
 
 
-def update_title_jeux(arr):
+def update_title(arr):
     try:
-        tmp = arr["title"]
+        _ = arr["title"]
         return re.sub(r'https://www.trictrac.net/jeu-de-societe/(.*)/.*', r'\1', arr["url"])
-    except:
+    except Exception:
         return re.sub(r'https://www.trictrac.net/jeu-de-societe/(.*)', r'\1', arr["url"])
 
 
@@ -64,7 +64,6 @@ def extract_editors(arr):
 
 
 def extract_distributors(arr):
-
     castings = parse_casting(arr["casting"])
     distributors = np.NaN
 
@@ -75,10 +74,93 @@ def extract_distributors(arr):
 
     return distributors
 
-def extract_years(arr):
+
+def extract_published_year(arr):
     match = re.search(r"\b\d{4}\b", arr["date_published"])
 
     if match:
         year = match.group()
         return year
+
     return np.NaN
+
+
+def parse_gameplay(gameplay):
+    return gameplay.split("|")
+
+
+def extract_players(arr):
+    nb_players = parse_gameplay(arr["gameplay"])[0]
+    min_p = 1
+    max_p = 99
+
+    if nb_players.startswith("jusqu'à"):
+        nb_players = nb_players.replace("jusqu'à ", "")
+        max_p = int(nb_players)
+
+    elif nb_players.startswith("à partir de"):
+        nb_players = nb_players.replace("à partir de ", "")
+        min_p = int(nb_players)
+
+    elif "à" in nb_players:
+        nb_players = nb_players.split("à")
+        min_p = int(nb_players[0])
+        max_p = int(nb_players[1])
+
+    else:
+        try:
+            min_p = int(nb_players)
+            max_p = min_p
+        except ValueError:
+            return np.NaN
+
+    return f"{min_p}-{max_p}"
+
+
+def extract_ages(arr):
+    age = parse_gameplay(arr["gameplay"])[1]
+    age = age.replace("\xa0", "")
+
+    min_a = 1
+    max_a = 99
+
+    if age.startswith("jusqu'à"):
+        age = age.replace("jusqu'à ", "")
+        max_a = int(age)
+
+    elif "ans et +" in age:
+        age = age.replace("ans et +", "")
+        min_a = int(age)
+
+    elif "à" in age:
+        age = age.split("à")
+        min_a = int(age[0])
+        max_a = int(age[1])
+
+    else:
+        try:
+            min_a = int(age)
+            max_a = min_a
+        except ValueError:
+            return np.NaN
+
+    return f"{min_a}-{max_a}"
+
+
+def extract_duration(arr):
+    duration = parse_gameplay(arr["gameplay"])[2]
+    duration = duration.replace("\xa0", "")
+
+    try:
+        durr = int(duration)
+    except ValueError:
+        durr = np.NaN
+
+    return durr
+
+
+def extract_categories(arr):
+    if type(arr["categories"]) == float:
+        return np.array([])
+
+    return np.array(arr["categories"].split("|"))
